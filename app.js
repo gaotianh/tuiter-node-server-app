2e1b100df5dfb2e37e5fb01ca006331176967e2e
gaotianh <gao.tianh99@gmail.com>
2022-11-29 14:14:02 -0500

    mongo db

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 // mongoose.connect('mongodb://localhost:27017/tuiter');
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
-    || 'mongodb://localhost:27017/tuiter'
+    // || 'mongodb://localhost:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 import express from 'express';
 import cors from 'cors'
